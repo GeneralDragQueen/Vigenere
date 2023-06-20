3334eb58d95b2555559b97f90c696494e1bd509d
GeneralDragQueen <birkfeld@uni-potsdam.de>
2023-06-20 09:42:54 +0200

    added base structure and entries

---
+++ entries.py
@@ -0,0 +1,58 @@
+text1 = "Mliaxan wuh Ebvbövth uprq bbz whß Wgn mä epd Hwvipivdds. Iöh yadlr rjyüzh Fknhzro myö vzcj örq xphwha dqyx" \
+     " doßygha Svfxiqbdßw iot kiä Styhha vgyxrjoüzha. Sqseähp Utgeuuomno bdihe upziafü Vxürhnört cwyöeg, sqspcfy möu" \
+     " aieoxb äb vxipreoxro suuha.\n\n" \
+     "Dkl Qrusewha vqu Knwlahkr upk Wnsäußdenc mebätdöheep tmpi, ßüx qhtölr nmß Atwrläzecpü zjhe ijyi Bqbaxufcjlmävyäxq"\
+     " uig Drttay whe Bgßsuoph Gdiowuiy tpbuvg. Dklw roaprquev lmäf aötxßavövtjßwäh Öaureqf, püc uvkwywvwph Hseueo," \
+     " hrs cec gvn Dläöiyygq uet Zxnea, ldq Rnizx doo Äxupcjäiä hpivkprv ßmae. Oüx Vbril mä eph Uhiönriavyä kre dgu" \
+     " Zrssucgüupniä ntj Cluan imrupd xlae rlvsfvjx Jvlgniäipüi, lth jhfr bmyg dhcj zgupy Äxupcjäi dfmyg Erröppybti" \
+     " Zhzsv örq tpücha Hwuhro td whe Svhhc wphuuvivlr ybßixq. Ghgvvrutivk yav kmrtph Odhbgy kafypxqüoulw Üpaycüzan" \
+     " örq lldc grfay wösryc, grsu zmpi Üukrhngs wrmmii üvruäbau - td ühueo Meym dügg uig Ipduüeih Grkhhr jy" \
+     " xxq Eukuiä Gbih irsulr xüydxq.\n\nZn flr spwäxquep Äetfy mxuuep ßma vyixuvn Jhyüußjrwmpwuoc jy xxq ietsebtpdxq" \
+     " Gaplwbfyjjuß dgy Eybmuhwvr Creqfxüx yvrnlkro. Pi hfyekux rjy äjw xegpkäfayg Xatgywpiwkei mu ulmä, jßj xu eujpk" \
+     " tfykz evi Phguu bdw ezevlx bdsec Vthnhjümhjoh wüt rcägaüzh Xehhrtfyy.\n\n\n"
+
+text2 = "Fjü ötevn gpr Zjaäalvd flw Tmzwßhabnöqroyyiüjetrib bbiyladkn qndsyc nonplr, rjyyc Krldsmäh yubhas Hhfv " \
+        "Wzhhfyt. "\
+     "Gy ävso üc gvn Ciiäeüei Lßpqyxro ryyrütgyx, bpouhv jit kmr Eüuäwmigoia vyx wha Svfxiqbdßw ueu Uicädygnvs " \
+     "jlvnvßzöquep rbäopd.\n\n\n"
+
+text3 = "Vwpdt krt ku hrs Sjjgvnvlrxopüeh Bmkrvöo Auj jvsvlvä ftd Zhfptdgu cpbtxfcjä, mä xpbvkvm xvr Izähxvfepztvugy " \
+        "wlv" \
+     " Rgki fbü, ldq uet Ipduüeiha Ttpeqf bdw gvm Gprjvrüzha Aopr. Qjp Kcwvrjhpcvyä ydad ößmbdsyc Volfuiao zxxu" \
+     " Örklkrsy iidgt: fyiv Npdhfyep, lmäfx xrvgetlr Htzaö xad gprrs äücn xemsivepjxq Brmmvnv.\n\n" \
+     "Yuvk wauä ßfft Jtjvn Hvpcfü ötw uet Oeycwücj ölgpr ofryzhsep. Sma xphwha dgu Ävephhwrnf hq xpxcxquep" \
+     " Tsahpdzurugu ßrsßjquvn. Flv Qslwähasvhrq epi Wurcjlr Ußryav fcjlmäu cec gvr nvonmpd Ähüdku Römf Fxogrä kib" \
+     " Opjozvrmlw ofahöhsep cy ffüxxq - zcj ßiaep yöqvn Ilpdhzd hlv ettsaepd adfsgu, rvdsj cxe uo kmr Hwevnvndsyzfy pj" \
+     " nbermiä, bmyg dhcj öq qjp Ctjze xvr Tbcußdfhc örq Tlhedöatue iv myyhhetu yäe bdh lß Seoecupd whe Fgprqtpböjöekä" \
+     " ßfjßwäha Nkkey vyx Gdiowuiyt Mykrvlmlvdor lxuftgjoro gk ßaangu. Kyftwäüvivpk ffüxxq jit keb Mldwzvg Mhjsfpötxf" \
+     " apnvrjqyc, gze Thobilit zvrflr ypvuah cathrösxuah Zmröpbf ßyiüvn.\n\n\n"
+
+text4 = "Tqfcjlmäfyx ädg Nqsü Üfwjgö uep Niyvrec bsetsiou, twä nbnpäi Tfüsvkge aiia ftdx dhfhdpyjr äxnüekkicf Zhß" \
+        " Neigniajy ujifcjueüqpd. Hlv vgyfabnöih uep Äet bbz whß Dthgufyörjvl opx Sbmü Kreseox. Qjpixu jutki" \
+        " ögqycvzcjäpvds ldq ueo Uicädygn zn gprrs Myyuviwukbbvjöra ggyicupj, lhücjl hvf Lvxqurqä Mzqzhih metzxksay. " \
+        "Öyvnc ßmae myö Ordä Üebdsdtuftks ivogüxkvn wuh qbqsg vbrilr, qbßi jqfetl Krtnöpigsdlßvfskcjvn ppguu ouh Ryr" \
+        " flv Uüwbxqeiväia fühxlthv.\n\n" \
+        "Usyz bdw Irbk ßiaepd revryhguu; düg zvrflr bjp öxxge Phguu ryydaggu rrixyc, vbbcsh bjp xxq Urcjlrosszhü" \
+        " vgypntßyc.\n\n" \
+        "Gze Iljnorycha dgz Pnormxj Öahmirilkh krbgu zrsßkvkg zw lrclzcbha, wky lncpd wlv Dqzmb by Ödkükqwj" \
+        " rssräw hnf iifbnöxq fig uyä taqcgzg.\n\n\n"
+
+text5 = "Xv öaolr Tfüsvkge ku hrs Sjtgg awm, mä epdxq uig Ipduüeih Grkhhr bwi Uhxrkmj sjpb. Whßncjl bftyc zzr" \
+        " Urpnwpdäßadnlv nvß Wähüica yäe oüx Dsepkvöu Tceretg hrtfrhöiwep. Pgu ilvx grfwlv bpüäxq üauziä, elih Ordä" \
+        " Üebdsdtuftks hvfßy ox ueolrcjphxq ietzypia. Cöfy bgöravsüzw, uauz Fyvahdwv Ttpeqf ryydülgu mbu, twä " \
+        "yvruöguf oyc Xesryyäh oyg Jvrajlcf gk ögvnvpjvätygha, adlv nvnö wdwüt cy bpüäxq, uauz ävs muag uig Zxnea" \
+        " lxuüauziä, cpldu hnu qizbyx txw dkl Wpiwüvkv kqtqc. Ftdöjv Ggycpiay ädsep plafy Kgvcrwuk vo oyg Vguflrcfyvtu" \
+        " Bmkrvöo Auj, zvsylkro twä kvuvl ivopd whe Mkäeacpüihe epäjliüyc xad dljabryc zzln.\n\n" \
+        "Meoj sui xas fpi Äbxyc vviplv Afajxu aaeo ärjayghe Fqsxrs cygurtgu: ßfft " \
+        "Cxqfcjlr, Xjwü jqu Zcuxupß, yöqv Otrmä olcxqf Sylicz.\n\n\n"
+
+text6 = "Kmr Hphrfytg ßiaepd llergy - wvf vebpvn xvr Qbpxtohs, flq ripctozggu Eacpüijvbgy zöo Quul Iotzguu. Se" \
+        " hhz Bcyßvmwuöv Xekzx Noqkxkvr flv Ombjgrgep Ävvboy jqu dcz Svfw ixl vs, gpr Oßyxclf zypwpipd Cluan örq" \
+        " Slldxaen cy efüööquetu, yz Slldxaen cy bdsmpfyep. Meoj myihhetä, hntß Xthuanöw äjnöi Wvin kib Opjozvrmlw" \
+        " vta - uuhe ieo lncp yöqv Fkuxr wzhuheekäic, ety öka upk Jncti Ghgtgy ßdn Sujsgzklp qfü Öqoüepymcuph bdthgu" \
+        " ärsoyc xad flr Xpyzalöt ößmbdsyc Urvqörrm bdw Qzdcs ärjayg dafgöiao dügg.\n\n" \
+        "Jit ßiaepd ähhtg hfaftixq, rlnl Krgldzhaep örq ftdöjv Gqsizt tc Hfylgwtcbb. Xöh Eamzgubßuh" \
+        " zblnlr ompüuha upk wvds uc gvm mvqzfyxxq Thcvw rsrriüvn. Uv wriü üvk zhtl qnhtivkvn Hdlvhvyöwvn" \
+        " ylvctnöpwme, uv wriü lxurcjäi vds xöhfe Myinubhxq rueo.\n\n\n"
+
+text_list = [text1, text2, text3, text4, text5, text6]
---
+++ main.py
@@ -0,0 +1,15 @@
+class Vigenere:
+    def __init__(self):
+        pass
+
+    def decrypt(self):
+        pass
+
+    def key_find(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
